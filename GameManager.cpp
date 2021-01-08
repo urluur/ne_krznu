11e588f77f931b95647e105e7a97544b569c74cd
Luka Ursic <urlu@pm.me>
2021-01-08 09:52:09 +0100

    objekt razreda gameManager bo tudi drzal ime igralca

--- GameManager.cpp
+++ GameManager.cpp
@@ -2,3 +2,9 @@
 GameManager::GameManager() {
 	//definiram privzete lastnosti
 }
+void GameManager::setPlayerName(std::string s) {
+	player_name = s;
+}
+std::string GameManager::getPlayerName() {
+	return player_name;
+}
--- GameManager.h
+++ GameManager.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <string>
 class GameManager{
+private:
+	std::string player_name;
 public:
 	GameManager();
+	void setPlayerName(std::string);
+	std::string getPlayerName();
 };
